1ed00dccbb22afb175dcde9375304ce25039f09d
Hezi <Hezireuveny93@gmail.com>
2023-06-11 17:57:51 +0300

    cahnged index file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import UserContextProvider from "./pages/context/context";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  // <React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   // <React.StrictMode>
+//   <UserContextProvider>
+//     <App />
+//   </UserContextProvider>
+//   // </React.StrictMode>
+// );
+
+ReactDOM.render(
   <UserContextProvider>
     <App />
-  </UserContextProvider>
-  // </React.StrictMode>
+  </UserContextProvider>,
+  document.getElementById("root")
 );
